06f5dee0b89e7cf2508b8eb51a00d001cb43452b
Tropped <tropped@gmail.com>
2023-03-08 01:23:11 +0100

    /leaderboard command

--- utils/discordLeaderboard.js
+++ utils/discordLeaderboard.js
@@ -3,6 +3,9 @@ const tierAPI = require("../api/tier");
 const ratingAPI = require("../api/rating");
 const guildAPI = require("../api/guild");
 const messageAPI = require("../api/message");
+const { WifiLeaderboardError } = require("../errors/wifiLeaderboard");
+
+const PLAYERS_PER_PAGE = 20;
 
 /**
  *
@@ -10,10 +13,15 @@ const messageAPI = require("../api/message");
  * @param {Role} role Role of the tier
  * @param {*} leaderboardInfo Object with the player + rating info of this tier
  */
-async function leaderboardEmbedBuilder(guild, role, leaderboardInfo) {
+async function leaderboardEmbedBuilder(guild, role, leaderboardInfo, page = 1) {
   let playerMessage = "";
+  const maxPages = leaderboardInfo ? Math.ceil(leaderboardInfo.length / PLAYERS_PER_PAGE) : 0;
 
-  if (leaderboardInfo) {
+  if (maxPages === 0) page = 0;
+
+  leaderboardInfo = leaderboardInfo?.slice((page - 1) * PLAYERS_PER_PAGE, PLAYERS_PER_PAGE * page);
+
+  if (leaderboardInfo && page > 0) {
     const playersInfo = await Promise.all(
       leaderboardInfo.map(async (row, i) => {
         let player = guild.members.cache.get(row.displayName);
@@ -21,24 +29,22 @@ async function leaderboardEmbedBuilder(guild, role, leaderboardInfo) {
           try {
             player = await guild.members.fetch(row.playerDiscordId);
           } catch (e) {
-            if (e instanceof DiscordAPIError) return null;
+            if (e instanceof DiscordAPIError) player = { displayName: "?????" };
             else throw e;
           }
         }
-        const playerName = guild.members.cache.get(row.playerDiscordId).displayName;
+        const playerName = player.displayName;
         const rating = row.rating;
         const promotionText = rating.promotion
           ? ` **[${rating.promotionWins} - ${rating.promotionLosses}]**`
           : "";
+        const playerIndex = (page - 1) * PLAYERS_PER_PAGE + i + 1;
 
-        return `${i + 1}. **${playerName}** _(${rating.score})_${promotionText}`;
+        return `${playerIndex}. **${playerName}** _(${rating.score})_${promotionText}`;
       })
     );
 
-    playerMessage = playersInfo
-      .filter((pi) => pi != null)
-      .slice(0, 20)
-      .join("\n");
+    playerMessage = playersInfo.join("\n");
   }
 
   if (playerMessage.trim() == "") playerMessage = "No hay nadie en esta tier... ¡de momento!";
@@ -47,6 +53,7 @@ async function leaderboardEmbedBuilder(guild, role, leaderboardInfo) {
     .setColor(role.color)
     .setTitle(`__**${role.name}**__`)
     .addFields({ name: "Jugadores", value: playerMessage })
+    .setFooter({ text: `Página ${page}/${maxPages}` })
     .setTimestamp();
 }
 
@@ -69,10 +76,42 @@ async function getLeaderboardEmbeds(guild) {
   return embeds;
 }
 
-async function getLeaderboardEmbed(guild, tierRoleId) {
-  const leaderboardInfo = await ratingAPI.getRatingsByTier(tierRoleId);
-  const role = await guild.roles.fetch(tierRoleId);
-  return await leaderboardEmbedBuilder(guild, role, leaderboardInfo);
+async function getLeaderboardEmbed(guild, tierRoleId, playerDiscordId, page) {
+  const { weighted } = await tierAPI.getTiers(guild.id);
+
+  const playerFocus = tierRoleId === null;
+  let tier = weighted.find((t) => t.roleId === tierRoleId);
+
+  if (!tier) {
+    tier = await ratingAPI.getPlayerTier(playerDiscordId, guild.id, true);
+    if (!tier || !weighted.some((t) => t.roleId === tier.roleId)) throw new WifiLeaderboardError();
+  }
+  const leaderboardInfo = await ratingAPI.getRatingsByTier(tier.roleId);
+
+  // GET PAGE
+  const maxLength = leaderboardInfo.length;
+  const maxPages = Math.ceil(maxLength / PLAYERS_PER_PAGE);
+  if (playerFocus) {
+    let playerIndex = leaderboardInfo.findIndex((li) => li.playerDiscordId === playerDiscordId);
+    if (playerIndex === -1) page = 1;
+    else {
+      playerIndex += 1;
+      page = Math.ceil(playerIndex / PLAYERS_PER_PAGE);
+    }
+  }
+
+  if (page < 1) page = 1;
+  if (page > maxPages) page = maxPages;
+
+  const role = await guild.roles.fetch(tier.roleId);
+  const embed = await leaderboardEmbedBuilder(guild, role, leaderboardInfo, page);
+
+  return {
+    embed,
+    page,
+    maxPages,
+    roleId: tier.roleId,
+  };
 }
 
 async function updateLeaderboard(guild) {
